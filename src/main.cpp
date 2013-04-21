718cf5aaa79cab84967881ac4d8516dffd380d1f
C. Mitch Carroll <c.mitch.carroll@gmail.com>
2013-04-20 21:00:05 -0500

    made soem changes to the camera code. again.

--- src/main.cpp
+++ src/main.cpp
@@ -73,21 +73,10 @@ void display(void)
   glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
   glLoadIdentity();
   glLightfv(GL_LIGHT0,GL_POSITION,sun_pos);
-  glRotatef(camera.rot.y,0,1,0);
-  glRotatef(camera.rot.x,1,0,0);
-  glRotatef(camera.rot.z,0,0,1);
+  glRotatef(-camera.rot.y,0,1,0);
+  glRotatef(-camera.rot.x,1,0,0);
+  glRotatef(-camera.rot.z,0,0,1);
   glTranslatef(camera.pos.x,camera.pos.y,camera.pos.z);
-
-  glPushMatrix();
-  glTranslatef(-Rocket.pos.x,-Rocket.pos.y,-Rocket.pos.z);
-  glRotatef(Earth.rot.y,0,1,0);
-  glRotatef(Earth.rot.x,1,0,0);
-  glRotatef(Earth.rot.z,0,0,1);
-  glColor3f(.25,.3,1);
-  glMaterialfv(GL_FRONT_AND_BACK,GL_DIFFUSE, earth_dif);
-  glDisable(GL_DEPTH_TEST);
-  glutWireSphere(Earth.radius,36,18);
-  glPopMatrix();
 
   glPushMatrix();
   glEnable(GL_DEPTH_TEST);
@@ -107,6 +96,16 @@ void display(void)
   glutSolidCone(2.5,5,10,5);
   glPopMatrix();
 
+  glPushMatrix();
+  glTranslatef(-Rocket.pos.x,-Rocket.pos.y,-Rocket.pos.z);
+  glRotatef(Earth.rot.y,0,1,0);
+  glRotatef(Earth.rot.x,1,0,0);
+  glRotatef(Earth.rot.z,0,0,1);
+  glColor3f(.25,.3,1);
+  glMaterialfv(GL_FRONT_AND_BACK,GL_DIFFUSE, earth_dif);
+  glDisable(GL_DEPTH_TEST);
+  glutSolidSphere(Earth.radius,36,18);
+  glPopMatrix();
 
   glutSwapBuffers();
 }
@@ -150,19 +149,20 @@ void keyboard(unsigned char key, int x, int y)
   switch(key){
   case 'a':
     camera.rot.y-=2;
-    camera.pos=camera.pos.rotate(-2,Y);
+    camera.pos=Vtx(0,0,Rocket.radius*zoom*-1).rotate(-camera.rot.y,Y).rotate(camera.rot.x,X);
     break;
   case 'd':
     camera.rot.y+=2;
-    camera.pos=camera.pos.rotate(2,Y);
+    camera.pos=Vtx(0,0,Rocket.radius*zoom*-1).rotate(-camera.rot.y,Y).rotate(camera.rot.x,X);
     break;
   case 'w':
     camera.rot.x-=2;
-    camera.pos=camera.pos.rotate(-2,X);
+    camera.pos=Vtx(0,0,Rocket.radius*zoom*-1).rotate(-camera.rot.y,Y).rotate(camera.rot.x,X);
     break;
   case 's':
     camera.rot.x+=2;
-    camera.pos=camera.pos.rotate(2,X);
+    camera.pos=Vtx(0,0,Rocket.radius*zoom*-1).rotate(-camera.rot.y,Y).rotate(camera.rot.x,X);
+    
     break;
   case ' ':
     cout << altitude(Rocket,Earth) << " " << Rocket.pos.x << " " << Rocket.pos.y << " " << Rocket.pos.z << endl;
